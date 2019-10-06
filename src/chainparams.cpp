965f2533ea90b8fd48744e95b94a246d69e4a9ab
qizf <qizf@outlook.com>
2019-10-06 22:02:17 +0800

    测试log

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -147,7 +147,7 @@ public:
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
         // vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
-
+        printf("测试哈哈哈");
         fMiningRequiresPeers = true;
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
